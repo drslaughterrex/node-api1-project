f5e7d05ba0fa35e9e84e1b6d9a8da852e722ff1f
drslaughterrex <drslaughterrex@gmail.com>
2021-05-31 15:30:59 -0500

    initial push

--- api/server.js
+++ api/server.js
@@ -1,3 +1,12 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require("express");
+const server = express();
+const port = 5000;
+
+server.get("/hello", (req, res) => {
+	res.json({ message: "hello" });
+});
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
